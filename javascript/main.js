0c482d873d55d0f4eeac955cfff3dde84d472158
williamge <williamgelhar@gmail.com>
2014-05-08 20:52:15 -0400

    Replacing the hardcoded input forms with JSON defined data and introducing AngularJS to the code.
    Each input form has been replaced by an ng-repeat template, the data and values are now taken from a (for now hardcoded in  JS) JSON definition.

---
+++ javascript/main.js
@@ -0,0 +1,41 @@
+var squatApp = angular.module( "squatApp", [] );
+
+squatApp.controller( "exercises", function( $scope, $http ) {
+	$scope.exercisesList = [
+		{
+			type : "Squat",
+			prefix : "s",
+			defaultWeight : 225,
+			defaultReps: 5,
+			defaultSets: 1
+		},
+		{
+			type : "Bench Press",
+			prefix : "b",
+			defaultWeight : 185,
+			defaultReps: 5,
+			defaultSets: 1
+		},
+		{
+			type : "Deadlift",
+			prefix : "d",
+			defaultWeight : 315,
+			defaultReps: 5,
+			defaultSets: 1
+		},		
+		{
+			type : "Barbell Row",
+			prefix : "r",
+			defaultWeight : 135,
+			defaultReps: 5,
+			defaultSets: 1
+		},
+		{
+			type : "Incline Bench",
+			prefix : "i",
+			defaultWeight : 95,
+			defaultReps: 5,
+			defaultSets: 1
+		},
+	];
+} );
