c1aba49a89188f1e5269320df8484e8fa67d0e19
williamge <williamgelhar@gmail.com>
2014-05-11 14:50:24 -0400

    Changing exercises.js to check if that global (ugh) config object exists before creating it.

--- json/exercises.js
+++ json/exercises.js
@@ -1,40 +1,41 @@
-var _squatData = {
+if (!window._squatData) {
+	var _squatData = {};
+}
 
-	"exercise" : [
-		{
-			"type" : "Squat",
-			"prefix" : "s",
-			"weight" : 225,
-			"reps": 5,
-			"sets": 1
-		},
-		{
-			"type" : "Bench Press",
-			"prefix" : "b",
-			"weight" : 185,
-			"reps": 5,
-			"sets": 1
-		},
-		{
-			"type" : "Deadlift",
-			"prefix" : "d",
-			"weight" : 315,
-			"reps": 5,
-			"sets": 1
-		},		
-		{
-			"type" : "Barbell Row",
-			"prefix" : "r",
-			"weight" : 135,
-			"reps": 5,
-			"sets": 1
-		},
-		{
-			"type" : "Incline Bench",
-			"prefix" : "i",
-			"weight" : 95,
-			"reps": 5,
-			"sets": 1
-		}
-	]
-};
+_squatData[ "exercise" ] = [
+	{
+		"type" : "Squat",
+		"prefix" : "s",
+		"weight" : 225,
+		"reps": 5,
+		"sets": 1
+	},
+	{
+		"type" : "Bench Press",
+		"prefix" : "b",
+		"weight" : 185,
+		"reps": 5,
+		"sets": 1
+	},
+	{
+		"type" : "Deadlift",
+		"prefix" : "d",
+		"weight" : 315,
+		"reps": 5,
+		"sets": 1
+	},		
+	{
+		"type" : "Barbell Row",
+		"prefix" : "r",
+		"weight" : 135,
+		"reps": 5,
+		"sets": 1
+	},
+	{
+		"type" : "Incline Bench",
+		"prefix" : "i",
+		"weight" : 95,
+		"reps": 5,
+		"sets": 1
+	}
+]
