6b7e31b51d239f36f7c9112ff54896e86742ad8a
williamge <williamgelhar@gmail.com>
2014-05-19 17:33:17 -0400

    Adding in a local copy of require.js.
    Just linking to it from their site is a bad idea, it's not a CDN and that location can go down and take down the site.

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,5 @@
 requirejs.config({
+        urlArgs: "bust=" + (new Date()).getTime(),
     "baseUrl": "javascript",
     "paths": {
       "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
